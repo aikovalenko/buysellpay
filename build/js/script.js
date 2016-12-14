90978c99d5e70d05302a749c2ed09da4c22b2770
aikovalenko <kovalenko.ai@yandex.ru>
2016-12-14 20:43:36 +0100

    Initital

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,33 @@
+(function($){
+    jQuery.fn.lightTabs = function(options){
+
+        var createTabs = function(){
+            tabs = this;
+            i = 0;
+
+            showPage = function(i){
+                $(tabs).children("div").children("div").css({"opacity": "0", "z-index": "-1"});
+                $(tabs).children("div").children("div").eq(i).css({"opacity": "1", "z-index": "1"});
+                $(tabs).children("ul").children("li").removeClass("active");
+                $(tabs).children("ul").children("li").eq(i).addClass("active");
+            }
+
+            showPage(0);
+
+            $(tabs).children("ul").children("li").each(function(index, element){
+                $(element).attr("data-page", i);
+                i++;
+            });
+
+            $(tabs).children("ul").children("li").click(function(){
+                showPage(parseInt($(this).attr("data-page")));
+            });
+        };
+        return this.each(createTabs);
+    };
+})(jQuery);
+
+
 $(document).ready(function() {
     $('.js-slider-products').slick({
         dots: false,
@@ -27,6 +57,36 @@ $(document).ready(function() {
             }
         ]
     });
+    $('.js-fragmented-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
     $('.js-slider-banners').slick({
         dots: false,
         infinite: true,
@@ -42,7 +102,66 @@ $(document).ready(function() {
             }
         ]
     });
+    $('.js-product-feedback__slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 980,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
     $(".button-collapse").sideNav({
-        draggable: true
+        // draggable: true
+    });
+    $(".tabs").lightTabs();
+    $(".thumbs img").click(function() {
+        event.preventDefault();
+        var block = $(".demo-trigger");
+        var image = $(this).data("image-normal");
+        var zoom = $(this).data("image-large");
+        block.attr("src", image);
+        block.attr("data-zoom", zoom);
+        new Drift(demoTrigger, {
+            paneContainer: paneContainer,
+            inlinePane: false
+        });
+    });
+    var demoTrigger = document.querySelector('.demo-trigger');
+    var paneContainer = document.querySelector('.product-card__desc');
+
+    new Drift(demoTrigger, {
+        paneContainer: paneContainer,
+        inlinePane: false
+    });
+
+    function close_accordion_section() {
+        jQuery('.accordion .accordion-section-title').removeClass('active');
+        jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+    }
+
+    jQuery('.accordion-section-title').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = jQuery(this).attr('href');
+
+        if(jQuery(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            jQuery(this).addClass('active');
+            // Open up the hidden content panel
+            jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
     });
 });
--- src/js/libs.js
+++ src/js/libs.js
@@ -1,3 +1,6 @@
 //= ../libs/jquery/dist/jquery.min.js
 //= ../libs/slick-carousel/slick/slick.min.js
 //= ../libs/materialize/dist/js/materialize.min.js
+//= ../libs/fancybox/source/jquery.fancybox.pack.js
+//= ../libs/luminous-master/dist/Luminous.min.js
+//= ../libs/drift-master/dist/Drift.min.js
